c438b147befbaa9c509dcaa83f67b5a56d09ea6a
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-30 23:57:43 +0400

    feat: work with all the request

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/', views.TodoList.as_view(), name='todo_list'),
+    path('todos/create/', views.TodoCreate.as_view(), name='todo_create'),
+    path('todos/<int:id>/update/', views.TodoUpdate.as_view(), name='todo_update'),
+    path('todos/<int:id>/delete/', views.TodoDelete.as_view(), name='todo_delete'),
+    path('delete-inactive-todos/', views.TodoDeleteInactive.as_view(), name='delete_inactive_todos')
 ]
--- todo/views.py
+++ todo/views.py
@@ -18,3 +18,29 @@ class TodoList(APIView):
         todos = Todo.objects.all()
         serializer = TodoSerializer(todos, many=True)
         return Response(serializer.data)
+
+class TodoUpdate(APIView):
+    def put(self, request, id):
+        todo = self.get_object(id)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TodoDelete(APIView):
+    def get_object(self, id):
+        try:
+            return Todo.objects.get(id=id)
+        except Todo.DoesNotExist:
+            raise Http404
+
+    def delete(self, request, id):
+        todo = self.get_object(id)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class TodoDeleteInactive(APIView):
+    def delete(self, request):
+        deleted_count = Todo.objects.filter(active=True).delete()
+        return Response({"message": f"{deleted_count[0]} inactive todos deleted."})
